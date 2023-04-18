f9170087c030cd2c87db476ec344a739b010c872
Jayashree <jayashreebheema@gmail.com>
2023-04-18 13:01:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("./db/conn");
+const router = require("./Routes/router");
+const PORT = 4002;
+
+// middleware
+app.use(express.json());
+app.use(
+  cors(
+    (origin = [
+      "http://localhost:3000",
+      "https://web-production-11881.up.railway.app/",
+    ])
+  )
+);
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server start at Port No :${PORT}`);
+});
